d73a94143727bbdcd5777ec32c9077b96200bdd4
nadipaca <nadipaca@mail.uc.edu>
2025-05-21 18:09:38 -0400

    update firebase config file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,27 +1,37 @@
 
 import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import {
   initializeAuth,
   getAuth,
   getReactNativePersistence
 } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID
-} from '@env';
+// import {
+//   FIREBASE_API_KEY,
+//   FIREBASE_AUTH_DOMAIN,
+//   FIREBASE_PROJECT_ID,
+//   FIREBASE_STORAGE_BUCKET,
+//   FIREBASE_MESSAGING_SENDER_ID,
+//   FIREBASE_APP_ID
+// } from '@env';
+
+// const firebaseConfig = {
+//   apiKey:FIREBASE_API_KEY,
+//   authDomain:FIREBASE_AUTH_DOMAIN,
+//   projectId:FIREBASE_PROJECT_ID,
+//   storageBucket:FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId:FIREBASE_MESSAGING_SENDER_ID,
+//   appId:FIREBASE_APP_ID,
+// };
 
 const firebaseConfig = {
-  apiKey:              FIREBASE_API_KEY,
-  authDomain:          FIREBASE_AUTH_DOMAIN,
-  projectId:           FIREBASE_PROJECT_ID,
-  storageBucket:       FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:   FIREBASE_MESSAGING_SENDER_ID,
-  appId:               FIREBASE_APP_ID,
+  apiKey: "AIzaSyBUdWNjWD90RW9PxpQAhZgo-tlQfWbGxdA",
+  authDomain: "playground-app-792c2.firebaseapp.com",
+  projectId: "playground-app-792c2",
+  storageBucket: "playground-app-792c2.firebasestorage.app",
+  messagingSenderId: "899534344073",
+  appId: "1:899534344073:web:ad5ebfa764ce035ebb9b58"
 };
 
 // 1) only initialize the App once
@@ -38,4 +48,5 @@ try {
   auth = getAuth(app);
 }
 
-export { auth };
+const db = getFirestore(app);
+export { auth, db };
