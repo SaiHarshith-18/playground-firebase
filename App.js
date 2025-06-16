dafa68f794f869dc63d309d9a5aa14a0c1165b7b
charish37 <Charishmanadipalli77@gmail.com>
2025-06-16 18:32:02 -0400

    Installed dependencies

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-// App.js
-import React, { useContext } from 'react';
+import React from 'react';
 import AppStack from './navigation/AppStack';
 import { AuthProvider } from './contexts/AuthContext';
-
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <AppStack />
-    </AuthProvider>
+    <PaperProvider>
+      <AuthProvider>
+        <AppStack />
+      </AuthProvider>
+    </PaperProvider>
   );
 }
