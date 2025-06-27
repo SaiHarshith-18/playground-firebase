3014ecfa567d6c0e8aaf6b2d677b65e3c317542e
charish37 <Charishmanadipalli77@gmail.com>
2025-06-27 13:35:44 -0400

    Add navigation container in the App

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import AppStack from './navigation/AppStack';
 import { AuthProvider } from './contexts/AuthContext';
+import CalloutModal from './screens/Events/CalloutModal';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { CalloutModalProvider } from './contexts/callOutModalContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { navigationRef } from './navigation/RootNavigation';
 
 export default function App() {
   return (
     <PaperProvider>
       <AuthProvider>
-        <AppStack />
+        <CalloutModalProvider>
+          <NavigationContainer ref={navigationRef}>
+            <AppStack />
+             <CalloutModal />
+          </NavigationContainer>
+         
+        </CalloutModalProvider>
       </AuthProvider>
     </PaperProvider>
   );
