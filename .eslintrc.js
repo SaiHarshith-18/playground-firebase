82fe9b54805fbcaf136ccd04d76e57785a62a84b
charish37 <Charishmanadipalli77@gmail.com>
2025-07-03 10:14:23 -0400

    add prettier and eslint to refactore the code

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  plugins: ['react', 'react-native', 'import'],
+  rules: {
+    // Customize your lint rules
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'prettier/prettier': 'error',
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+};
